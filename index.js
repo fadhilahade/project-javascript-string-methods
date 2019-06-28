05f06a937d6210f3ed1e1bb1933bd326718e16be
fadhilahade <dhila.alkautsar@gmail.com>
2019-06-28 11:31:07 +0700

    edit code

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-let editNewText = prompt('input the text you want to rotate')
-let newText = ' '
-let fixedText = ''
-let regex = /a-zA-Z/gi
+let editNewText = prompt("input the text you want to rotate");
+let newText = " ";
+let fixedText = " ";
 
-function rotate(editNewText) {
-    for (let i = 0; i < editNewText.length; i++) {
-        newText += String.fromCharCode(editNewText.charCodeAt(i) + 13);
+function rotate(newTextParameter) {
+    for (let i = 0; i < newTextParameter.length; i++) {
+
+        fixedText +=
+            newTextParameter.charCodeAt(i) >= 110 &&
+            newTextParameter.charCodeAt(i) <= 122 &&
+            newTextParameter.charCodeAt(i) >= 78 &&
+            newTextParameter.charCodeAt(i) <= 90 ?
+            newTextParameter.charCodeAt(i) - 13 + " " :
+            newTextParameter.charCodeAt(i) + 13 + " ";
     }
-    alert(`your name with rot13 is ${newText}`);
+    console.log(fixedText)
+    fixedText.split(" ").forEach(item => {
+        newText += String.fromCharCode(item);
+    })
+    alert(`your name with rot13 is ${newText}`)
 }
-rotate(editNewText)
+rotate(editNewText);
